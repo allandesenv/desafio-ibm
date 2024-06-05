c488d0833f00ab143318eeacfe16c01cb197bcf8
allandesenv <allandd8@gmail.com>
2024-06-05 10:52:54 -0300

    initial commit

---
+++ desafio-ibm/src/main/java/com/example/desafio_ibm/DesafioIbmApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_ibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioIbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioIbmApplication.class, args);
+	}
+
+}
